99541ea03ac1ea0d2af94d8c6b9cb273ccdf8333
Jonathan Dayton <jonathandayton23@gmail.com>
2017-12-04 10:05:19 -0700

    Try to turn the AE into a VAE

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -6,6 +6,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 INPUT_SIZE = 784
 CODE_SIZE = 20
 
+# Statistics the normal distribution should have
+MEAN = 0.0
+STDEV = 1.0
+
+
 def show_image(x, name="image"):
     # This assumes the input is a square image...
     width_height = int(INPUT_SIZE**0.5)
@@ -23,9 +28,12 @@ if __name__ == "__main__":
         outputs = fully_connected(code, INPUT_SIZE)
     show_image(outputs, "outputs")
     with tf.name_scope("optimizer"):
-        loss = tf.losses.mean_squared_error(inputs, outputs)
-        tf.summary.scalar("loss", loss)
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+        mse = tf.losses.mean_squared_error(inputs, outputs)
+        tf.summary.scalar("mse", mse)
+        # mean, var = tf.nn.moments(code, axis=[0])
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(mse)
+
 
 
     with tf.Session() as sess:
