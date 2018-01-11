8e22ab95c0b496c7083ffbeffe25b129d00cd1bc
Jonathan Dayton <jonathandayton23@gmail.com>
2018-01-11 16:23:50 -0700

    Load RNA-Seq data

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -2,9 +2,11 @@ from . import hide_warnings
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
 INPUT_SIZE = 784
 CODE_SIZE = 20
+BATCH_SIZE = 100
 
 # Statistics the normal distribution should have
 MEAN = 0.0
@@ -16,6 +18,15 @@ def show_image(x, name="image"):
     width_height = int(INPUT_SIZE**0.5)
     img = tf.reshape(x, [-1, width_height, width_height, 1])
     tf.summary.image(name, img, max_outputs=1)
+
+def mask(inputs):
+    mask_ = np.full_like(inputs, 0.5)
+    for i, x in enumerate(mask_):
+        curr = 0.0
+        for j, _ in enumerate(x):
+            mask_[i][j] = curr
+            curr += 1.0 / INPUT_SIZE
+    return inputs * mask_
 
 if __name__ == "__main__":
     mnist = input_data.read_data_sets("mnist_data", one_hot=True)
@@ -31,10 +42,7 @@ if __name__ == "__main__":
         mse = tf.losses.mean_squared_error(inputs, outputs)
         tf.summary.scalar("mse", mse)
         # mean, var = tf.nn.moments(code, axis=[0])
-
         optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(mse)
-
-
 
     with tf.Session() as sess:
         merged = tf.summary.merge_all()
@@ -42,7 +50,9 @@ if __name__ == "__main__":
         tf.global_variables_initializer().run()
 
         for i in range(10000):
-            batch_inputs, _ = mnist.train.next_batch(100)
+            batch_inputs, _ = mnist.train.next_batch(BATCH_SIZE)
+            if i % 2 == 0:
+                batch_inputs = mask(batch_inputs)
             summary, _ = sess.run([merged, optimizer], feed_dict={
                 inputs: batch_inputs
                 })
---
+++ src/load_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gzip
+
+class RNASeq(object):
+    def __init__(self):
+        self.data = self.load_data()
+        self.cur_ix = 0
+
+    def load_data(self):
+        data = []
+        with gzip.open("./data/RNASeq.txt.gz", "r") as infile:
+            header = infile.readline()
+            for line in infile:
+                line = line.strip("\n").split("\t")[1:]
+                line = [float(x) for x in line]
+                data.append(line)
+        return np.array(data)
+
+    def next_batch(self, batch_size):
+        if self.cur_ix + batch_size < len(self.data):
+            np.random.shuffle(self.data)
+            self.cur_ix = 0
+        batch = self.data[self.cur_ix:self.cur_ix + batch_size]
+        self.cur_ix += batch_size
+        return batch
+
+if __name__ == "__main__":
+    data = RNASeq()
+    print(data.data.shape)
