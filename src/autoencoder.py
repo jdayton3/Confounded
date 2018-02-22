c90c936f1d6bad7fffbeea11838891276bcb5ef1
Jonathan Dayton <jonathandayton23@gmail.com>
2018-02-22 15:49:57 -0700

    Adversarial AE working (kind of)

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -9,6 +9,7 @@ import numpy as np
 INPUT_SIZE = 784
 CODE_SIZE = 200
 BATCH_SIZE = 100
+NUM_TARGETS = 2
 
 # Statistics the normal distribution should have
 MEAN = 0.0
@@ -28,28 +29,57 @@ if __name__ == "__main__":
     mnist = input_data.read_data_sets("mnist_data", one_hot=True)
 
     # Autoencoder net
-    inputs = tf.placeholder(tf.float32, [None, INPUT_SIZE])
-    show_image(inputs, "inputs")
-    with tf.name_scope("encoding"):
-        code = fully_connected(inputs, CODE_SIZE)
-    with tf.name_scope("decoding"):
-        outputs = fully_connected(code, INPUT_SIZE)
-    show_image(outputs, "outputs")
-    with tf.name_scope("optimizer"):
-        mse = tf.losses.mean_squared_error(inputs, outputs)
-        tf.summary.scalar("mse", mse)
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(mse)
+    with tf.name_scope("autoencoder"):
+        inputs = tf.placeholder(tf.float32, [None, INPUT_SIZE])
+        show_image(inputs, "inputs")
+        with tf.name_scope("encoding"):
+            encode1 = fully_connected(inputs, 512, activation_fn=tf.nn.relu)
+            encode2 = fully_connected(encode1, 256, activation_fn=tf.nn.relu)
+            code = fully_connected(encode2, CODE_SIZE, activation_fn=tf.nn.relu)
+        with tf.name_scope("decoding"):
+            decode1 = fully_connected(code, 256, activation_fn=tf.nn.relu)
+            decode2 = fully_connected(decode1, 512, activation_fn=tf.nn.relu)
+            outputs = fully_connected(decode2, INPUT_SIZE, activation_fn=tf.nn.sigmoid)
+        show_image(outputs, "outputs")
+
+    # Discriminator net
+    with tf.name_scope("discriminator"):
+        targets = tf.placeholder(tf.float32, [None, NUM_TARGETS])
+        fc1 = fully_connected(outputs, 256)
+        fc2 = fully_connected(fc1, 128)
+        fc3 = fully_connected(fc2, 64)
+        fc4 = fully_connected(fc3, 8)
+        fc5 = fully_connected(fc4, 8)
+        classification = fully_connected(fc5, NUM_TARGETS, activation_fn=tf.nn.sigmoid)
+        with tf.name_scope("optimizer"):
+            d_loss = tf.losses.mean_squared_error(classification, targets)
+            tf.summary.scalar("mse", d_loss)
+            d_optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(d_loss)
+
+    with tf.name_scope("autoencoder"):
+        with tf.name_scope("optimizer"):
+            mse = tf.losses.mean_squared_error(inputs, outputs)
+            tf.summary.scalar("mse", mse)
+            loss =  mse + (tf.ones_like(d_loss) - d_loss)
+            optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
     with tf.Session() as sess:
         merged = tf.summary.merge_all()
-        writer = tf.summary.FileWriter("log", sess.graph)
+        writer = tf.summary.FileWriter("log/adversarial", sess.graph)
         tf.global_variables_initializer().run()
 
-        for i in range(1000):
+        for i in range(10000):
             batch_inputs, _ = mnist.train.next_batch(BATCH_SIZE)
             if i % 2 == 0:
                 batch_inputs = mask(batch_inputs)
-            summary, _ = sess.run([merged, optimizer], feed_dict={
-                inputs: batch_inputs
-                })
+                target = [[1.0, 0.0]] * BATCH_SIZE
+            else:
+                target = [[0.0, 1.0]] * BATCH_SIZE
+            target = np.array(target)
+            summary, disc, _, _ = sess.run([merged, outputs, optimizer, d_optimizer], feed_dict={
+                inputs: batch_inputs,
+                targets: target,
+            })
             writer.add_summary(summary, i)
+
+            #TODO: change the masking layer, trim/normalize the weights
