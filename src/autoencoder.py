75a35e62d27450771b9d37406fe11c8ecffd714e
Jonathan Dayton <jonathandayton23@gmail.com>
2018-06-01 17:00:38 -0600

    Add higher-level noise and try discriminator

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,6 +1,7 @@
 # pylint: disable=E1129,E0611,E1101
 
 from . import hide_warnings
+from .adjustments import Noise
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected, batch_norm
 from tensorflow.examples.tutorials.mnist import input_data
@@ -60,7 +61,8 @@ if __name__ == "__main__":
     # Discriminator net
     with tf.name_scope("discriminator"):
         targets = tf.placeholder(tf.float32, [None, NUM_TARGETS])
-        fc1 = fully_connected(outputs, 256)
+        # fc1 = fully_connected(outputs, 256)
+        fc1 = fully_connected(inputs, 256)
         fc2 = fully_connected(fc1, 128)
         fc3 = fully_connected(fc2, 64)
         fc4 = fully_connected(fc3, 8)
@@ -80,19 +82,31 @@ if __name__ == "__main__":
 
     with tf.Session() as sess:
         merged = tf.summary.merge_all()
-        writer = tf.summary.FileWriter("log/add_noise", sess.graph)
+        writer = tf.summary.FileWriter("log/order_2_discriminator_0.001", sess.graph)
         tf.global_variables_initializer().run()
         mask = add_noise
+
+        df = 0.001
+        noiser_0 = Noise((INPUT_SIZE,), discount_factor=df)
+        noiser_1 = Noise((INPUT_SIZE,), discount_factor=df)
 
         for i in range(10000):
             batch_inputs, _ = mnist.train.next_batch(BATCH_SIZE)
             if i % 2 == 0:
-                batch_inputs = mask(batch_inputs)
+                # batch_inputs = mask(batch_inputs)
+                adj_batch_inputs = []
+                for x in batch_inputs:
+                    adj_batch_inputs.append(noiser_0.adjust(x))
                 target = [[1.0, 0.0]] * BATCH_SIZE
             else:
+                adj_batch_inputs = []
+                for x in batch_inputs:
+                    adj_batch_inputs.append(noiser_1.adjust(x))
                 target = [[0.0, 1.0]] * BATCH_SIZE
+            batch_inputs = adj_batch_inputs
             target = np.array(target)
-            summary, disc, _, _ = sess.run([merged, outputs, optimizer, d_optimizer], feed_dict={
+            # summary, disc, _, _ = sess.run([merged, outputs, optimizer, d_optimizer], feed_dict={
+            summary, disc, _ = sess.run([merged, outputs, d_optimizer], feed_dict={
                 inputs: batch_inputs,
                 targets: target,
             })
