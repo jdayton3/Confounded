a55001511707b8df8fd709d82a538adfd686bb0b
Jonathan Dayton <jonathandayton23@gmail.com>
2018-09-12 14:57:25 -0600

    Predict digit for MNIST images pre- and post-adjustment

--- metrics/classifiers/true_class.py
+++ metrics/classifiers/true_class.py
@@ -1,2 +1,20 @@
 """Predict the true class of MNIST digits with a SVC classifier.
 """
+
+from sklearn.svm import SVC
+from .random_forests import cross_validate
+
+if __name__ == "__main__":
+    FILES = ["nonadjusted", "batches2", "confounded_digit"]
+    for name in FILES:
+        print(name)
+        path = f"./data/tidy_{name}.csv"
+        print(
+            cross_validate(
+                path,
+                meta_cols=["Sample", "Batch", "Digit"],
+                predict="Digit",
+                model=SVC, # SVC takes about 40 minutes to run.
+                times=5
+            )
+        )
