348497505bee933b4a15eb4f93e2846eeff89679
Jonathan Dayton <jonathandayton23@gmail.com>
2018-10-09 11:13:14 -0600

    Add more todos

---
+++ scripts/metrics_pipeline.py
@@ -0,0 +1,9 @@
+"""
+TODO: make a big, nice pipeline that takes in one input file & runs it
+through Confounded, ComBat, and SVA, saves those output files, and
+calculates the following metrics:
+- how long did it take?
+- for each algorithm, what's the MSE (before - after)?
+- what is our Random Forests & SVA accuracy for batch before & after?
+- what is our "" accuracy for true signal before & after?
+"""
