da5c3a6aa29fb1dec02cca91da1261838dbe2654
Jonathan Dayton <jonathandayton23@gmail.com>
2018-10-12 16:23:27 -0600

    Add more notes to metrics pipeline

--- scripts/metrics_pipeline.py
+++ scripts/metrics_pipeline.py
@@ -7,3 +7,14 @@ calculates the following metrics:
 - what is our Random Forests & SVA accuracy for batch before & after?
 - what is our "" accuracy for true signal before & after?
 """
+
+INPUT_FILE = "../data/GSE40292_copy.csv"
+BATCH_COL = "Batch"
+
+# TODO: Run through Confounded & Save output file
+
+# TODO: Classify on batch, record accuracies & time
+
+# TODO: Classify on true signal, record accuracies & time
+
+# TODO: save / output metrics
