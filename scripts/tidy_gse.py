308bf122a1af71c9efb3f1893c43b0c12197ea2e
Jonathan Dayton <jonathandayton23@gmail.com>
2018-12-24 22:37:26 -0700

    Add clinical variables to GSE data

--- scripts/tidy_gse.py
+++ scripts/tidy_gse.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
-gseid = "GSE40292"
+gseid = "GSE37199"
 path = "data/avery/{}/".format(gseid)
 
 # clinical = pd.read_table("{}{}_Clinical.txt".format(path, gseid))
 unadj = pd.read_table("{}{}_Expression_BatchUnadjusted.txt.gz".format(path, gseid)).rename(columns={"Unnamed: 0": "Sample"})
-batch = pd.read_table("{}{}_Expression_Batch.txt".format(path, gseid)).drop("sample.1", axis="columns")
-batch = batch.dropna(subset=['Batch'])
-try:
-    batch["Batch"] = batch['Batch'].astype(int)
-except:
-    pass
-tot = pd.concat([batch.set_index("sample"), unadj.set_index("Sample")], axis="columns", join="inner").reset_index().rename(columns={"index": "Sample"})
+clinical = pd.read_table("{}{}_Clinical.txt".format(path, gseid), index_col=False)
 
-tot.to_csv("{}tidy.csv".format(path), index=False)
+tot = pd.concat([clinical.set_index("SampleID"), unadj.set_index("Sample")], axis="columns", join="inner").reset_index().rename(columns={"index": "Sample"})
+
+tot.to_csv("{}clinical.csv".format(path), index=False)
