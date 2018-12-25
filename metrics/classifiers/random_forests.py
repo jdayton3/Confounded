2e32777a43693df8adb256024d75e1273356807e
Jonathan Dayton <jonathandayton23@gmail.com>
2018-12-24 22:35:49 -0700

    Fix output from classification

--- metrics/classifiers/random_forests.py
+++ metrics/classifiers/random_forests.py
@@ -6,21 +6,27 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from .csvread import CSVData
 
-def cross_validate(path, meta_cols=None, predict="Batch", times=100, folds=5, model=RandomForestClassifier):
-    classifier = model()
-    print(f"Using classifier: {classifier.__class__}")
+def cross_validate(path, meta_cols=None, predict="Batch", times=100, folds=5, model=RandomForestClassifier, **kwargs):
+    classifier = model(**kwargs)
     data = CSVData(path, meta_cols, predict)
     accuracies = []
-    start = time.time()
+    elapsed_times = []
     for _ in range(times):
-        print(f"{_+1}/{times}", end="\r")
-        accuracies += list(cross_val_score(
-            classifier, data.X, data.Y, cv=folds, scoring="accuracy"
-        ))
-    print()
-    print(f"Time elapsed: {time.time() - start} seconds")
-    return sum(accuracies) / len(accuracies)
-
+        start = time.time()
+        try:
+            accuracies += list(cross_val_score(
+                classifier, data.X, data.Y, cv=folds, scoring="accuracy"
+            ))
+            elapsed_times.append(time.time() - start)
+        except ValueError as e:
+            print("Something didn't work with file {}, column {}, and classifier {}:".format(
+                path, predict, model.__name__)
+            )
+            print(str(e))
+    return {
+        "accuracies": accuracies,
+        "times": elapsed_times
+    }
 
 if __name__ == "__main__":
     INPUT_PATH = "../../data/tidy_combat.csv"
--- metrics/classifiers/true_class.py
+++ metrics/classifiers/true_class.py
@@ -2,35 +2,86 @@
 """
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from .random_forests import cross_validate
+
+class Logger(object):
+    def __init__(self, log_file):
+        self.log_file = log_file
+        self.values = {
+            "path": [],
+            "model": [],
+            "predict": [],
+            "baseline": [],
+            "iteration": [],
+            "accuracy": [],
+            "time_elapsed": [],
+        }
+
+    def log(self, path, model, predict_col, baseline, accuracies, times_elapsed):
+        for i, (accuracy, time) in enumerate(zip(accuracies, times_elapsed)):
+            self.values["path"].append(path)
+            self.values["model"].append(model.__name__)
+            self.values["predict"].append(predict_col)
+            self.values["baseline"].append(baseline)
+            self.values["iteration"].append(i)
+            self.values["accuracy"].append(accuracy)
+            self.values["time_elapsed"].append(time)
+
+    def save(self):
+        pd.DataFrame(self.values).to_csv(self.log_file, index=False)
+
 
 def baseline(path, column):
     df = pd.read_csv(path)
     return df[column].value_counts().max() / len(df)
 
 if __name__ == "__main__":
-    # FILES = ["nonadjusted", "batches2", "confounded_digit", "batches_balanced", "confounded_balanced"]
-    # PATHS = [f'./data/tidy_{name}.csv' for name in FILES]
     PATHS = [
-        # "data/avery/GSE25507/tidy_confounded.csv",
-        "data/avery/GSE37199/tidy.csv",
-        "data/avery/GSE37199/tidy_confounded.csv",
+        "data/avery/GSE37199/clinical.csv",
+        "data/avery/GSE37199/clinical_confounded.csv",
         "data/avery/GSE37199/tidy_combat.csv",
-        # "data/tidy_batches_balanced_combat.csv",
-        # "data/avery/GSE39582/tidy.csv",
-        # "data/avery/GSE40292/tidy.csv",
     ]
-    PREDICT = "Batch"
+    LEARNERS = [
+        (RandomForestClassifier, {}),
+        (MLPClassifier, {"hidden_layer_sizes": tuple([5]*10), "max_iter": 1000}),
+        (GaussianNB, {}),
+        (KNeighborsClassifier, {}),
+        (SVC, {"kernel": "rbf"})
+    ]
+    PREDICT = [
+        'Batch',
+        'centre',
+        'plate',
+        'replicate',
+        'Stage',
+    ]
+
+    logger = Logger("./data/metrics/accuracies.csv")
     for path in PATHS:
-        print(path)
-        print("Baseline: {}".format(baseline(path, PREDICT)))
-        print(
-            cross_validate(
-                path,
-                predict=PREDICT,
-                # model=SVC, # SVC takes about 40 minutes to run on MNIST.
-                model=RandomForestClassifier,
-                times=5
-            )
-        )
+        for column in PREDICT:
+            try:
+                baseline_acc = baseline(path, column)
+            except KeyError:
+                print("Column {} not in file {}. Skipping.".format(column, path))
+                continue
+            for learner in LEARNERS:
+                cv_scores = cross_validate(
+                    path,
+                    predict=column,
+                    model=learner[0],
+                    times=5,
+                    **learner[1]
+                )
+                logger.log(
+                    path,
+                    learner[0],
+                    column,
+                    baseline_acc,
+                    cv_scores["accuracies"],
+                    cv_scores["times"]
+                )
+    logger.save()
