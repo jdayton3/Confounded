b9566434723185c5b8ddf0da8c1f8c3e96c3a8dc
Jonathan Dayton <jonathandayton23@gmail.com>
2018-12-26 17:19:57 -0700

    Fix code size script

--- scripts/code_size.sh
+++ scripts/code_size.sh
@@ -11,6 +11,7 @@ for i in 10 50 100 500 1000 5000 10000 20000
 do
     printf -v padded "%05d" $i
     python -m src.autoencoder $input_path \
-        --output-file ${output_folder}size_${padded}.csv \
+        --output-file ${output_folder}size_${padded}_plate.csv \
         --code-size $i
+        --batch-col "plate"
 done
