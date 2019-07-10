4b2f77c422e3b0b3f868de8500818981f8d77599
Jonathan Dayton <jonathandayton23@gmail.com>
2019-07-10 15:45:39 -0600

    Remove unnecessary files

--- scripts/configurations.sh
+++
@@ -1,7 +0,0 @@
-#bash "scripts/ae_layers.sh"
-#bash "scripts/code_size.sh"
-#bash "scripts/features.sh"
-#bash "scripts/lambda_vals.sh"
-bash "scripts/layers.sh"
-bash "scripts/mnist.sh"
-#bash "scripts/scaling.sh"
