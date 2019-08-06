03d705fc9150933fdc5a15704436288876e2afaf
srp33 <stephen_piccolo@byu.edu>
2019-08-06 06:13:50 -0600

    Fixing issue with pandas Index when saving

--- confounded/reformat.py
+++ confounded/reformat.py
@@ -21,8 +21,7 @@ def to_csv(df, path, tidy=False, meta_cols=None, column_order=None):
     if column_order:
         df = df[column_order]
 
-    #df.to_csv(path, index=pd.Index.empty)
-    df.to_csv(path, index=None)
+    df.to_csv(path, index=None, header=True)
 
 def _add_meta_cols(df, meta_cols):
     if meta_cols is None:
